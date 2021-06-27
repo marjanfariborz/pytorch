479a00729799705c71d2ecc066cf9256f5bb030d
Zeina Migeed <migeedz@fb.com>
2021-06-27 13:49:53 -0700

    task 1: annotate
    ghstack-source-id: 01a1bfb33602f5ba046bc1ab7f3a46013bdbad66
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/60621
    
    run tests with python3.7+
    
    ghstack-source-id: 01a1bfb33602f5ba046bc1ab7f3a46013bdbad66
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/60789

---
+++ torch/fx/annotate.py
@@ -0,0 +1,13 @@
+from torch.fx.proxy import Proxy
+
+def annotate(val, type):
+    # val could be either a regular value (not tracing)
+    # or fx.Proxy (tracing)
+    if isinstance(val, Proxy):
+        if val.node.type:
+            raise RuntimeError("type already exists")
+        else:
+            val.node.type = type
+        return val
+    else:
+        return val
